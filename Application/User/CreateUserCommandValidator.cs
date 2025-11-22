64e9edd6418a8427cdb75bdc036b8e2ff6d2eb1e
Mahdi Tahanian <mahdi.tahanian@live.com>
2025-11-22 15:03:48 +0330

    Fix MaxLength validation architectural test

---
+++ Application/User/CreateUserCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Application.User.Commands;
+
+namespace Application.User
+{
+    public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserCommandValidator()
+        {
+            RuleFor(x => x.Name)
+          .NotEmpty()
+          .MaximumLength(200);
+
+            RuleFor(x => x.Username)
+                .NotEmpty()
+                .MaximumLength(200);
+
+            RuleFor(x => x.Email)
+                .MaximumLength(200);
+        }
+    }
+}
